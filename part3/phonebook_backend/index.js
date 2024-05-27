90b337ecd1f405f2f49cb388a2d9dd551d9eeba1
Ruben Carbal <rubengio111@gmail.com>
2024-05-26 19:28:30 -0500

    uibuild

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -81,7 +81,7 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
